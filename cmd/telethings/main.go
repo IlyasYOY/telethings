11dc6e8e95fb1515006b1180a3e4fcbd6205be6f
Ilya Ilyinykh <IlyasYOY@gmail.com>
2026-02-22 15:15:55 +0300

    feat: add main.go with basic bot setup and graceful shutdown handling

---
+++ cmd/telethings/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os/signal"
+	"syscall"
+
+	"github.com/IlyasYOY/telethings/internal/bot"
+	"github.com/IlyasYOY/telethings/internal/config"
+	"github.com/IlyasYOY/telethings/internal/opener"
+)
+
+func main() {
+	cfg, err := config.FromEnv()
+	if err != nil {
+		log.Fatalf("config: %v", err)
+	}
+
+	b, err := bot.New(cfg, opener.MacOSOpener{})
+	if err != nil {
+		log.Fatalf("bot: %v", err)
+	}
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	if err := b.Run(ctx); err != nil {
+		log.Printf("bot stopped: %v", err)
+	}
+}
